d439c5f21a28bdc74d6dc41792597c8d0fde095c
Igor P <ipergenitsa@gmail.com>
2022-10-16 23:10:44 +0300

    wrap controls into a div

--- src/components/CircleOfBalance.vue
+++ src/components/CircleOfBalance.vue
@@ -1,12 +1,14 @@
 <template>
-    <svg viewBox="0 0 100 100" width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-        <polygon :points="points" />
-        <circle :cx=radius :cy=radius :r=radius />
-    </svg>
-    <div :key="stat" v-for="(stat, index) in stats">
-        <label>{{index + 1}}</label>
-        <input type="range" v-model="stat.value" min="0" max="100" />
-        <span>{{stat.value}}</span>
+    <div>
+        <svg viewBox="0 0 100 100" width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+            <polygon :points="points" />
+            <circle :cx=radius :cy=radius :r=radius />
+        </svg>
+        <div :key="stat" v-for="(stat, index) in stats">
+            <label>{{index + 1}}</label>
+            <input type="range" v-model="stat.value" min="0" max="100" />
+            <span>{{stat.value}}</span>
+        </div>
     </div>
 </template>
 
@@ -17,7 +19,7 @@ import valueToPoint from '../common-functions'
 export default {
     data() {
         return {
-            stats: Array.from({length: 10}, it => ({ value: this.generateValue() })),
+            stats: Array.from({ length: 10 }, it => ({ value: this.generateValue() })),
             radius: 50
         }
     },
